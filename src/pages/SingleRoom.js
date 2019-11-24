488cf0f9d374aae14b2e0a9e598556a85992411c
ArnaudDuthoit <43751449+ArnaudDuthoit@users.noreply.github.com>
2019-11-24 15:22:13 +0100

    First commit

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+export default class SingleRoom extends
+    Component {
+    render(){
+        return(
+            <div>
+                Bonjour de la page Single Room
+            </div>
+        )
+    }
+}
+
