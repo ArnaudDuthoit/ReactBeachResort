d519c94c09510e4518dd3370297e2c940c23f464
ArnaudDuthoit <43751449+ArnaudDuthoit@users.noreply.github.com>
2019-11-27 13:59:14 +0100

    V1.0 fonctionnelle

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -73,6 +73,28 @@ export default function RoomsFilter({rooms}) {
                 <input type='range' name="price" min={minPrice} max={maxPrice} id="price"  value={price} className='form-control' onChange={handleChange} />
             </div>
             {/* end prix */}
+            {/* Taille */}
+            <div className="form-group">
+                <label htmlFor="size"> Taille </label>
+                <div className='size-inputs'>
+                    <input type="number" name="minSize" id='size' value={minSize} onChange={handleChange} className='size-input'/>
+                    <input type="number" name="maxSize" id='size' value={maxSize} onChange={handleChange} className='size-input'/>
+                </div>
+            </div>
+            {/* end Taille */}
+
+            {/* extras*/}
+            <div className="form-group">
+               <div className="single-extra">
+                   <input type="checkbox" name="breakfast" id="breakfast" checked={breakfast} onChange={handleChange}/>
+                   <label htmlFor="breakfast">petit dej</label>
+               </div>
+                <div className="single-extra">
+                    <input type="checkbox" name="pets" id="pets" checked={pets} onChange={handleChange}/>
+                    <label htmlFor="pets">Animaux</label>
+                </div>
+            </div>
+            {/* end extras */}
 
 
         </form>
--- src/context.js
+++ src/context.js
@@ -66,7 +66,7 @@ class RoomProvider extends Component {
 
     handleChange = event => {
         const target = event.target;
-        const value = event.type === 'checkbox' ? target.checked : target.value;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
 
         const name = event.target.name;
         this.setState({
@@ -100,6 +100,19 @@ class RoomProvider extends Component {
 
         //filter par prix
         tempRooms = tempRooms.filter(room => room.price <= price);
+
+        //filter par taille
+        tempRooms =  tempRooms.filter(room => room.size >= minSize && room.size <= maxSize);
+
+        //filter breakfast
+       if (breakfast){
+           tempRooms = tempRooms.filter(room =>room.breakfast === true)
+       }
+
+        //filter par animaux
+        if (pets){
+            tempRooms = tempRooms.filter(room =>room.pets === true)
+        }
 
         //change le state
         this.setState({
